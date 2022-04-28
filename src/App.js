244c537c290647b2dd26de968b417fc337d8edbb
Isaac <43579566+isaacdyess@users.noreply.github.com>
2022-04-27 19:54:40 -0500

    Stripped down React template.
    Removed unnecessary files/features from React template to avoid clutter.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Budget Buddy!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Future home of Isaac's budget.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
